10aef974426eacc304d317d7bb6c759e5717ac9e
Ckancord33 <niccord33@gmail.com>
2025-03-01 08:34:13 -0500

    Implementacion del fastAlgorithm

--- src/main/scala/main.scala
+++ src/main/scala/main.scala
@@ -18,8 +18,8 @@ def unit(): Unit = {
     val numberDigitB: Int = numberDigits(b, 0) / 2
     val nFinal: Int = if (numberDigitA >= numberDigitB) numberDigitA else numberDigitB //PREGUNTAR AL PROFESOR...
 
-    def defTopPart(d: Int, nDigits: Int): Int = d / Math.pow(10, nDigits).toInt;
-    def defBottomPart(e: Int, numberDigit: Int): Int = e % math.pow(10, numberDigit).toInt;;
+    def defTopPart(d: Int, nDigits: Int): Int = d / Math.pow(10, nDigits).toInt
+    def defBottomPart(e: Int, numberDigit: Int): Int = e % math.pow(10, numberDigit).toInt
 
     val topPartA = defTopPart(a,nFinal)
     val bottomPartA = defBottomPart(a,nFinal)
@@ -35,7 +35,44 @@ def unit(): Unit = {
     algorithmMethod();
   }
 
-  println(splitAlgorithm(20,-5))
+  println(splitAlgorithm(2,50000))
+
+  def fastAlgorithm(a: Int, b: Int): Int = {
+    def numberDigits(c: Int, nDigits: Int): Int = if (c == 0) nDigits else numberDigits(c / 10, nDigits + 1)
+
+    def abs(x: Int) = if(x >= 0) a else -x
+
+    if (abs(a) < 10 && abs(b) < 10) return a * b
+
+    val numberDigitA: Int = numberDigits(a, 0) / 2
+    val numberDigitB: Int = numberDigits(b, 0) / 2
+    val nFinal: Int = if (numberDigitA >= numberDigitB) numberDigitA else numberDigitB
+
+    def defTopPart(d: Int, nDigits: Int): Int = d / Math.pow(10, nDigits).toInt
+
+    def defBottomPart(e: Int, numberDigit: Int): Int = e % math.pow(10, numberDigit).toInt
+
+    val topPartA = defTopPart(a, nFinal)
+    val bottomPartA = defBottomPart(a, nFinal)
+    val topPartB = defTopPart(b, nFinal)
+    val bottomPartB = defBottomPart(b, nFinal)
+
+    val topRecycled = fastAlgorithm(topPartA,topPartB)
+    val bottomRecycled = fastAlgorithm(bottomPartA, bottomPartB)
+
+    def firstStep: Int = Math.pow(10, nFinal * 2).toInt * topRecycled
+
+    def secondStep: Int = Math.pow(10, nFinal).toInt * (topRecycled + bottomRecycled - fastAlgorithm((topPartA - bottomPartA),(topPartB - bottomPartB)))
+
+    def thirdStep: Int = bottomRecycled
+
+    def algorithmMethod(): Int = firstStep + secondStep + thirdStep
+
+    algorithmMethod()
+
+  }
+
+  println(fastAlgorithm(340,-230).toString)
 
 
   def suma(a:Int): Int => Int = {
